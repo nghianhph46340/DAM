c3adde2fb94a1d76a411ee2e5d3e5012e81c9619
nghianhph46340 <146502380+nghianhph46340@users.noreply.github.com>
2024-01-24 17:18:40 +0700

    hehe
    hehe

---
+++ src/DbConnect/DBConnect1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package DbConnect;
+
+import java.sql.*;
+/**
+ *
+ * @author admin
+ */
+public class DBConnect1 {
+
+    static String url = "jdbc:sqlserver://;serverName=localhost;databaseName=ASM_SOF203";
+    static String username = "sa";
+    static String password = "12345678";
+    public static void main(String[] args) throws SQLException {
+        getConnection();
+    }
+    
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(url, username, password);
+    }
+    
+}
